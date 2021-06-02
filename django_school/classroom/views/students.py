94b0f10240a7dcac7bccb4642bc83658698c0a41
Vildan16 <vildan.asckarov@gmail.com>
2021-06-02 23:54:46 +0300

    Revert "base"
    This reverts commit 61df45ce5862252903de5c015b74335d30126d87, reversing
    changes made to f74c30dfbe5ad19028d206ca10a743d8cfa600f5.

--- django_school/classroom/views/students.py
+++ django_school/classroom/views/students.py
@@ -259,7 +259,7 @@ class StudentMaps(View):
 
         data = {
             "name": "Курс",
-            "size": 8207,
+            "size": 5507,
             "children": []
         }
 
@@ -268,13 +268,13 @@ class StudentMaps(View):
             for j in range(pars[f"p{i}"]):
                 a.append(
                     {
-                        "name": f"Параграф {j + 1}", "size": 7507
+                        "name": f"Параграф {j + 1}", "size": 5507
                     }
                 )
 
             data["children"].append(
                 {
-                    "name": f"Раздел {i}", "size": 7907, "children": a
+                    "name": f"Раздел {i}", "size": 5507, "children": a
                 }
             )
             a = []
