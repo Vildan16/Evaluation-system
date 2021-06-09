2608b5d06e7c872093fd870e38b44652a4a16bf8
kblaze <ahunovedik@mail.ru>
2021-06-09 17:23:09 +0300

    final

--- django_school/django_school/settings.py
+++ django_school/django_school/settings.py
@@ -14,23 +14,17 @@ import os
 
 from django.contrib.messages import constants as messages
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'd$pxg6fisc4iwzk&vz^s_d0lkf&k63l5a8f!obktw!jg#4zvp3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -79,9 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_school.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,7 +80,8 @@ DATABASES = {
     }
 }
 
-LANGUAGE_CODE = 'ru'
+
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -100,12 +92,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+
+# Custom Django auth settings
 
 AUTH_USER_MODEL = 'classroom.User'
 
@@ -118,6 +115,8 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 
+# Messages built-in framework
+
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-secondary',
     messages.INFO: 'alert-info',
@@ -127,8 +126,12 @@ MESSAGE_TAGS = {
 }
 
 
+# Third party apps configuration
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Base url to serve media files
 MEDIA_URL = '/media/'
 
+# Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
